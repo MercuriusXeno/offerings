b3fbc7baaf0e63508bb05e921d8adc29248fae78
Xeno <1145764+MercuriusXeno@users.noreply.github.com>
2025-11-02 19:19:52 -0600

    nitpicking splicing var names and toned down some baselines for balance

--- biome/splice.lua
+++ biome/splice.lua
@@ -2,12 +2,12 @@ dofile_once("data/scripts/lib/utilities.lua")
 
 -- cursed biome splice for altar_left which hates me and wants me to be unhappy.
 if ModImageMakeEditable then
-    local splice = function(original, splice, splice_x, splice_y)
+    local splice = function(original, splice, x_origin, y_origin)
         local id, _, _ = ModImageMakeEditable(original, 0, 0)
-        local splice_id, end_x, end_y = ModImageMakeEditable(splice, 0, 0)
-        for y = splice_y, splice_y + end_y - 1 do
-            for x = splice_x, splice_x + end_x - 1 do
-                local c = ModImageGetPixel(splice_id, x - splice_x, y - splice_y)
+        local splice_id, width, height = ModImageMakeEditable(splice, 0, 0)
+        for y = y_origin, y_origin + height - 1 do
+            for x = x_origin, x_origin + width - 1 do
+                local c = ModImageGetPixel(splice_id, x - x_origin, y - y_origin)
                 local r, g, b, a = color_abgr_split(c)
                 -- clear or black [material] gets ignored - don't splice negative spaces
                 if a > 0 and r + g + b > 0 then ModImageSetPixel(id, x, y, c) end
